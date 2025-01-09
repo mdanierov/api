71f969c50766fcc8af922bb8d2f805b7ab1f9129
Mirzorasul Danierov <md@netinventors.de>
2025-01-09 22:45:45 +0100

    Add Route Classe

---
+++ app/Routing/Route.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DMirzorasul\Api\Routing;
+
+class Route
+{
+    public static array $routes = [];
+
+    public static function get(string $pattern, string $class, string $method): void
+    {
+        if (!isset(self::$routes['GET'])) {
+            self::$routes['GET'] = [];
+        }
+
+        self::$routes['GET'][$pattern] = [ 'class' => $class, 'method' => $method ];
+    }
+
+    public static function post(string $pattern, string $class, string $method): void
+    {
+        if (!isset(self::$routes['POST'])) {
+            self::$routes['POST'] = [];
+        }
+
+        self::$routes['POST'][$pattern] = [ 'class' => $class, 'method' => $method ];
+    }
+
+    public static function patch(string $pattern, string $class, string $method): void
+    {
+        if (!isset(self::$routes['PATCH'])) {
+            self::$routes['PATCH'] = [];
+        }
+
+        self::$routes['PATCH'][$pattern] = [ 'class' => $class, 'method' => $method ];
+    }
+
+    public static function getMatchedController(string $method, string $path): ?array
+    {
+        if (!isset(self::$routes[$method][$path])) {
+            return null;
+        }
+
+        return self::$routes[$method][$path];
+    }
+}
