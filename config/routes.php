2c6dc8cea774281b9a1e2c8fa78d97d116b3dfd5
Mirzorasul Danierov <md@netinventors.de>
2025-01-09 22:44:52 +0100

    update Routes

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,7 @@
 <?php
 
+use DMirzorasul\Api\Routing\Route;
 use DMirzorasul\Api\Controller\TaskController;
-use DMirzorasul\Api\Routing\Routing;
 
-$routing = new Routing();
-
-$routing->get('/tasks', TaskController::class, 'index');
-$routing->post('/tasks/store', TaskController::class, 'store');
-$routing->post('/tasks/update', TaskController::class, 'update');
-$routing->post('/tasks/delete', TaskController::class, 'delete');
-
-return $routing;
+Route::get('/tasks', TaskController::class, 'index');
+Route::post('/tasks/store', TaskController::class, 'store');
