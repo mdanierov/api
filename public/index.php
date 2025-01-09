12d8ed5838ae39f935322e4ef760c18e0544c524
Mirzorasul Danierov <md@netinventors.de>
2025-01-09 22:44:45 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,23 @@
 <?php
 
+use DI\Container;
 use DMirzorasul\Api\App;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/routes.php';
 
-$routing = require './config/routes.php';
+/** @var Container $container */
+$container = require dirname(__DIR__) . '/config/bootstrap.php';
 
-$app = new App($routing);
+/** @var App $app */
+$app = $container->get(App::class);
+
 try {
-    $app->run();
-} catch (Exception $exception) {
-    var_dump($exception->getMessage());
-    die();
+    /** @var \Symfony\Component\HttpFoundation\Response $response */
+    $response = $container->call([ $app, 'handle' ]);
+} catch (\DMirzorasul\Api\Exceptions\ValidationException $exception) {
+    $response = new JsonResponse($exception->getErrors(), $exception->status);
 }
 
-exit();
+$response->send();
