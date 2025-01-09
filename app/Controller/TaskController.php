3b6cae2f08310b2a769f358b29cb2a71a12138c5
Mirzorasul Danierov <md@netinventors.de>
2025-01-09 22:46:45 +0100

    Update Taskcontroller

--- app/Controller/TaskController.php
+++ app/Controller/TaskController.php
@@ -2,120 +2,29 @@
 
 namespace DMirzorasul\Api\Controller;
 
-use DMirzorasul\Api\Validations\Task\TaskCreateRequest;
-use Doctrine\DBAL\Exception;
+use DMirzorasul\Api\Validations\Tasks\CreateTaskValidator;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\ConstraintViolationInterface;
 
-class TaskController extends Controller
+class TaskController
 {
-    /**
-     * @throws Exception
-     */
-    public function index(Request $request): Response
+    public function __construct(
+        private readonly Connection $connection,
+    ) {
+    }
+
+    public function index(): Response
     {
         $tasks = $this->connection->fetchAllAssociative('SELECT * FROM `tasks`');
 
         return new JsonResponse($tasks);
     }
 
-    /**
-     * @throws Exception
-     */
-    public function store(TaskCreateRequest $request): JsonResponse
+    public function store(CreateTaskValidator $validator): Response
     {
-        $title = $request->title;
+        dd($validator);
 
-        $violations = $this->validator->validate($title, [
-            new NotBlank(),
-        ]);
-
-        if ($violations->count() > 0) {
-            $messages = array_map(fn(ConstraintViolationInterface $violation) => $violation->getMessage(),
-                iterator_to_array($violations));
-
-            return new JsonResponse($messages, Response::HTTP_BAD_REQUEST);
-        }
-
-        $sql = "INSERT INTO `tasks` (`title`) VALUES (?);";
-
-        $this->connection->executeStatement($sql, [
-            $title,
-        ]);
-
-        return new JsonResponse([
-            'message' => 'task created',
-        ]);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function update(Request $request): JsonResponse
-    {
-        $data = [];
-
-        $data['title'] = $request->request->get('title');
-        $data['id']    = $request->request->get('id');
-
-        $errors = [];
-        foreach ($data as $key => $value) {
-            $violations = $this->validator->validate($value, [
-                new NotBlank(),
-            ]);
-
-             $messages = array_map(fn(ConstraintViolationInterface $violation) => $violation->getMessage(),
-                iterator_to_array($violations));
-
-             if (count($messages) > 0) {
-                 $errors[$key] = $violations;
-             }
-        }
-
-        if (count($errors) > 0) {
-            return new JsonResponse($errors, Response::HTTP_BAD_REQUEST);
-        }
-
-        $sql = 'UPDATE `tasks` SET `title` = ? WHERE `id` = ?';
-
-        $isSuccessfully = (bool) $this->connection->executeStatement($sql, [
-            $data['title'],
-            $data['id']
-        ]);
-
-        return new JsonResponse([
-            'successfully' => $isSuccessfully
-        ]);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function delete(Request $request): JsonResponse {
-        $id = $request->request->get('id');
-
-        $violations = $this->validator->validate($id, [
-            new NotBlank(),
-        ]);
-
-        if ($violations->count() > 0) {
-            $messages = array_map(fn(ConstraintViolationInterface $violation) => $violation->getMessage(),
-                iterator_to_array($violations));
-
-            return new JsonResponse($messages, Response::HTTP_BAD_REQUEST);
-        }
-
-        $sql = 'DELETE FROM `tasks` WHERE `id` = ?';
-
-        $isSuccessfully = (bool) $this->connection->executeStatement($sql, [
-            $id
-        ]);
-
-        return new JsonResponse([
-            'successfully' => $isSuccessfully
-        ]);
+        return new JsonResponse();
     }
 }
