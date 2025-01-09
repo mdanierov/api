fc7c4ec86aa9e7c082548affc84aa2b2f69d3853
Mirzorasul Danierov <md@netinventors.de>
2025-01-09 22:46:13 +0100

    Add Env helper

---
+++ app/Env/Env.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace DMirzorasul\Api\Env;
+
+class Env
+{
+    public const VAR_NAME_REGEX = '(?i:_?[A-Z][A-Z0-9_]*+)';
+
+    private array $values = [];
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct(
+        private readonly string $path
+    ) {
+        $this->load();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function load(): void
+    {
+        if (!is_readable($this->path) || is_dir($this->path)) {
+            throw new \Exception('Path is wrong!!!  --- ' . $this->path);
+        }
+
+        $data = file_get_contents($this->path);
+        if (str_starts_with($data, "\xEF\xBB\xBF")) {
+            throw new \Exception('Loading files starting with a byte-order-mark (BOM) is not supported.');
+        }
+
+        $data   = str_replace([ "\r\n", "\r" ], "\n", $data);
+        $cursor = 0;
+        $end    = strlen($data);
+        if (preg_match('/(?:\s*+(?:#[^\n]*+)?+)++/A', $data, $match, 0, $cursor)) {
+            $cursor = strlen($match[0]);
+        }
+
+        while ($cursor < $end) {
+            if (!preg_match('/(export[ \t]++)?(' . self::VAR_NAME_REGEX . ')/A', $data, $matches, 0, $cursor)) {
+                throw new \Exception('Invalid character in variable name');
+            }
+
+            $cursor += strlen($matches[0]);
+
+            if ($cursor === $end || "\n" === $data[$cursor] || '#' === $data[$cursor]) {
+                if ($matches[1]) {
+                    throw new \Exception('Unable to unset an environment variable');
+                }
+
+                throw new \Exception('Missing = in the environment variable declaration');
+            }
+
+            if (' ' === $data[$cursor] || "\t" === $data[$cursor]) {
+                throw new \Exception('Whitespace characters are not supported after the variable name');
+            }
+
+            if ('=' !== $data[$cursor]) {
+                throw new \Exception('Missing = in the environment variable declaration');
+            }
+
+            $cursor++;
+            $value = '';
+
+            while ($cursor < $end && $data[$cursor] !== "\n" && $data[$cursor] !== "\t") {
+                $value .= $data[$cursor++];
+            }
+
+            $this->set($matches[2], $value);
+            if (preg_match('/(?:\s*+(?:#[^\n]*+)?+)++/A', $data, $match, 0, $cursor)) {
+                $cursor += strlen($match[0]);
+            }
+        }
+    }
+
+    public function get(string $name): mixed
+    {
+        return $this->values[$name];
+    }
+
+    public function set(string $name, mixed $value): void
+    {
+        $this->values[$name] = $value;
+    }
+
+    public function getString(string $name): string
+    {
+        $value = $this->get($name);
+        if (!is_string($value)) {
+            throw new \Exception("$name is not found or not string");
+        }
+
+        return $value;
+    }
+}
