85151badb7238188e21f9dab8a5b2db65ab3373b
Mirzorasul Danierov <md@netinventors.de>
2025-01-16 00:57:50 +0100

    add routes

--- config/routes.php
+++ config/routes.php
@@ -6,3 +6,7 @@ use DMirzorasul\Api\Controller\TaskController;
 Route::get('/', TaskController::class, 'index');
 Route::get('/tasks', TaskController::class, 'index');
 Route::post('/tasks/store', TaskController::class, 'store');
+// TODO: Add params via route
+Route::post('/tasks/update', TaskController::class, 'update');
+// TODO: Add params via route
+Route::post('/tasks/delete', TaskController::class, 'delete');
